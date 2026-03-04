c885c842cfbe2d6fb850c406b93d889fd482d9ca
Sky Chen <sky.chen@salesforce.com>
2026-03-03 23:54:00 -0800

    update hard-coded url

--- webapp/app.py
+++ webapp/app.py
@@ -1,20 +1,25 @@
 import os
 
-from flask import Flask, render_template, send_file, send_from_directory
+from flask import Flask, Response, render_template, request, send_file
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+def _base_url() -> str:
+    return request.host_url.rstrip("/")
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", base_url=_base_url())
 
 
 @app.route("/install.sh")
 def install_sh():
-    return send_from_directory("static", "install.sh", mimetype="text/plain")
+    content = render_template("install.sh", base_url=_base_url())
+    return Response(content, mimetype="text/plain")
 
 
 @app.route("/install.py")
